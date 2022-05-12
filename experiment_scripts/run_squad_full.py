c0eddc1638a02009340c40cf4a7e0b7034490ab4
Julius-Syvis <55212122+Julius-Syvis@users.noreply.github.com>
2022-05-12 15:41:26 +0300

    update experiment scripts

--- experiment_scripts/run_squad_full.py
+++ experiment_scripts/run_squad_full.py
@@ -1,9 +1,10 @@
-from models.core import get_supported_model_factories, get_xlm
+from models.core import get_xlm, get_roberta_base, get_big_bird, get_electra_base
 from tasks.core import get_squad_v2
 from training.core import TrainConfig, MultipleTrainSequencer
 
 if __name__ == "__main__":
-    sequencer = MultipleTrainSequencer(get_supported_model_factories(), [get_squad_v2()],
+    sequencer = MultipleTrainSequencer([get_roberta_base(), get_electra_base(), get_big_bird(), get_xlm()],
+                                       [get_squad_v2()],
                                        TrainConfig(
                                            do_few_sample=False,
                                            experiment_name=f"full_samples_squad"))
