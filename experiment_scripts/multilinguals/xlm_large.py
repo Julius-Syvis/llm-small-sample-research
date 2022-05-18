5899317bfad6340ae6ab319f615d7abc97581ec3
Julius-Syvis <55212122+Julius-Syvis@users.noreply.github.com>
2022-05-18 22:44:47 +0300

    fix equal dataset size issues

--- experiment_scripts/multilinguals/xlm_large.py
+++ experiment_scripts/multilinguals/xlm_large.py
@@ -4,27 +4,27 @@ from training.config import TrainConfig
 from training.core import MultipleTrainSequencer
 
 if __name__ == "__main__":
-    sequencer = MultipleTrainSequencer(
-        [get_xlm_roberta_large()],
-        [get_wikiann_en(), get_wikiann_lt()],
-        TrainConfig(
-            do_train=False,
-            custom_train_sample_count=0,
-            experiment_name=f"en_lt_comparison_none"))
-    sequencer.train()
+    # sequencer = MultipleTrainSequencer(
+    #     [get_xlm_roberta_large()],
+    #     [get_wikiann_en(), get_wikiann_lt()],
+    #     TrainConfig(
+    #         do_train=False,
+    #         custom_train_sample_count=0,
+    #         experiment_name=f"en_lt_comparison_none"))
+    # sequencer.train()
+    #
+    # sequencer = MultipleTrainSequencer(
+    #     [get_xlm_roberta_large()],
+    #     [get_wikiann_en(), get_wikiann_lt()],
+    #     TrainConfig(
+    #         custom_train_sample_count=10,
+    #         custom_eval_step=10,
+    #         custom_max_step=200,
+    #         early_stopping_patience=3,
+    #         experiment_name=f"en_lt_comparison_10"))
+    # sequencer.train()
 
-    sequencer = MultipleTrainSequencer(
-        [get_xlm_roberta_large()],
-        [get_wikiann_en(), get_wikiann_lt()],
-        TrainConfig(
-            custom_train_sample_count=10,
-            custom_eval_step=10,
-            custom_max_step=200,
-            early_stopping_patience=3,
-            experiment_name=f"en_lt_comparison_10"))
-    sequencer.train()
-
-    for sample_size in [100, 1000, 10000]:
+    for sample_size in [10000]:
         sequencer = MultipleTrainSequencer(
             [get_xlm_roberta_large()],
             [get_wikiann_en(), get_wikiann_lt()],
